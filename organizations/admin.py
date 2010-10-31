b6e844e8b21f93161881efc0a40127323ef8383f
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-31 14:03:40 -0400

    Added organization types to the admin

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,9 +1,14 @@
 from django.contrib			import admin
 from organizations.models	import *
 
+class OrganizationTypeAdmin(admin.ModelAdmin):
+	list_display = ['name', 'description']
+	pass
+
 class OrganizationAdmin(admin.ModelAdmin):
 	list_display = ['name', 'homepage', 'organization_type', 'modified']
 	search_fields = ['name']
 	pass
 	
+admin.site.register(OrganizationType, OrganizationTypeAdmin)	
 admin.site.register(Organization, OrganizationAdmin)
