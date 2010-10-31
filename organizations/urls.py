392eaf07a77f18e9eb9cf8e0bc5d3abe88f1d7ab
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-31 15:26:28 -0400

    URLs on the homepage

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls.defaults import *
 
+from models import Organization
+
 urlpatterns = patterns('',
     
-    # @todo
+    # Edit an organization
+    url(r'^organizations/(?P<slug>[-\w]+)/edit/$', 'organizations.views.organization_edit', name='organization_edit'),
+    # Detailed organization
+    url(r'^organizations/(?P<slug>[-\w]+)/$', 'organizations.views.organization_detailed', name='organization_detailed'),
+    # All organizations listing
+    url(r'^organizations/$', 'organizations.views.organization_list', name='organization_list')
     
 )
--- organizations/views.py
+++ organizations/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+def organization_list(request):
+    pass
+    
+def organization_detailed(request):
+    pass
+    
+def organization_edit(request):
+    pass
