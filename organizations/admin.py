53d7b51059e6ab02c6f636a4558d270465a4bcbc
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-31 14:07:47 -0400

    Added product types to the admin

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,14 +1,26 @@
-from django.contrib			import admin
-from organizations.models	import *
+from django.contrib         import admin
+from organizations.models   import *
 
 class OrganizationTypeAdmin(admin.ModelAdmin):
-	list_display = ['name', 'description']
-	pass
+    list_display = ['name', 'description']
+    pass
 
 class OrganizationAdmin(admin.ModelAdmin):
-	list_display = ['name', 'homepage', 'organization_type', 'modified']
-	search_fields = ['name']
-	pass
-	
-admin.site.register(OrganizationType, OrganizationTypeAdmin)	
-admin.site.register(Organization, OrganizationAdmin)
+    list_display = ['name', 'homepage', 'organization_type', 'modified']
+    search_fields = ['name']
+    pass
+    
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    pass
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'homepage', 'organization', 'product_type']
+    search_fields = ['name', 'product_type']
+    pass
+
+admin.site.register(OrganizationType, OrganizationTypeAdmin)    
+admin.site.register(Organization, OrganizationAdmin)    
+    
+admin.site.register(ProductType, ProductTypeAdmin)    
+admin.site.register(Product, ProductAdmin)
