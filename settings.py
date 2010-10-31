61bde10aeb025a5a10b69bcc23fc35ccbc45ce4c
Max Cutler <max@maxcutler.com>
2010-10-31 11:42:26 -0400

    Default database settings.

--- settings.py
+++ settings.py
@@ -20,17 +20,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'mediabase',                      # Or path to database file if using sqlite3.
-#         'USER': 'camayak',                      # Not used with sqlite3.
-#         'PASSWORD': 'camayak',                  # Not used with sqlite3.
-#         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-#     }
-# }
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
