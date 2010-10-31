be624f76c473ebb5cf9b04f48312e9ffec0b984d
Max Cutler <max@maxcutler.com>
2010-10-31 11:30:57 -0400

    Added database dump from initial DBpedia scraping.

--- settings.py
+++ settings.py
@@ -12,13 +12,24 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mediabase',                      # Or path to database file if using sqlite3.
+        'NAME': 'newsanalyzr.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'mediabase',                      # Or path to database file if using sqlite3.
+#         'USER': 'camayak',                      # Not used with sqlite3.
+#         'PASSWORD': 'camayak',                  # Not used with sqlite3.
+#         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
