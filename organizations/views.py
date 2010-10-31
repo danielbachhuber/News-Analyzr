8ff4149016261952692afed880c11e1d77f887e2
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-31 15:49:27 -0400

    Outline for organization_detailed view

--- organizations/views.py
+++ organizations/views.py
@@ -1,10 +1,18 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+from organizations.models import *
 
 def organization_list(request):
     pass
     
-def organization_detailed(request):
-    pass
+def organization_detailed(request, slug):
+    
+    organization = get_object_or_404(Organization, slug=slug)
+    
+    return render_to_response('organizations/organization_detailed.html', {
+        'organization': organization,
+    }, context_instance=RequestContext(request))
     
 def organization_edit(request):
     pass
