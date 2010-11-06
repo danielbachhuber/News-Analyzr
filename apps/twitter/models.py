6574f03d937c9ffeeaaa77509381d8ca3b6ea010
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-11-06 14:03:58 -0400

    Model for logging meta information on Twitter account

--- apps/twitter/models.py
+++ apps/twitter/models.py
@@ -12,5 +12,25 @@ class TwitterAccount(models.Model):
     
     def __unicode__(self):
         return self.username
+        
+class TwitterAccountInfo(models.Model):
+    account = models.ForeignKey(TwitterAccount)
+    
+    # Information available on each Twitter account
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    url = models.URLField(verify_exists=False)
+    bio = models.TextField()
+    following = models.PositiveIntegerField()
+    followers = models.PositiveIntegerField()
+    total_updates = models.PositiveIntegerField()
+    
+    fetched = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-fetched']
+    
+    def __unicode__(self):
+        return self.account.username
 
 
