fad8ecb6018352e63b1694a3750b3b1ea4388359
Max Cutler <max@maxcutler.com>
2010-11-06 19:41:35 -0400

    Changed Activity model to be tied to a User and require explicit creation.

--- apps/activities/models.py
+++ apps/activities/models.py
@@ -7,13 +7,5 @@ class Activity(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.TextField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    # user = models.ForeignKey(User)
+    user = models.ForeignKey(User)
     created = models.DateTimeField(auto_now=True)
-
-def save_activity(sender, instance, created, **kwargs):
-    if sender._meta.app_label == 'organizations':
-        activity = Activity()
-        activity.content_type = ContentType.objects.get_for_model(sender)
-        activity.object_id = instance.pk
-        activity.save()
-models.signals.post_save.connect(save_activity)
