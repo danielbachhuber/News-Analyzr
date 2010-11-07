78481e619605f0fdd95e20ed61400ecdd0e71a60
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-11-06 23:07:49 -0400

    Fix URL conf for organizations

--- apps/organizations/urls.py
+++ apps/organizations/urls.py
@@ -5,10 +5,10 @@ from models import Organization
 urlpatterns = patterns('',
     
     # Edit an organization
-    url(r'^organizations/(?P<slug>[-\w]+)/edit/$', 'apps.organizations.views.organization_edit', name='organization_edit'),
+    url(r'^(?P<slug>[-\w]+)/edit/$', 'apps.organizations.views.organization_edit', name='organization_edit'),
     # Detailed organization
-    url(r'^organizations/(?P<slug>[-\w]+)/$', 'apps.organizations.views.organization_detailed', name='organization_detailed'),
+    url(r'^(?P<slug>[-\w]+)/$', 'apps.organizations.views.organization_detailed', name='organization_detailed'),
     # All organizations listing
-    url(r'^organizations/$', 'apps.organizations.views.organization_list', name='organization_list')
+    url(r'^$', 'apps.organizations.views.organization_list', name='organization_list')
     
 )
